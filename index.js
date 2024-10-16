5e74a0b615bc5d7f69c4190a91134b7478fc46a7
mmbrrains <miriamrains0219@gmail.com>
2024-10-16 00:58:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ let rnd = Math.random()
 
 if (rnd < 0.25) {
     div.style.fontSize = '11.8px'
-    div.innerHTML = 'We\'re still the & in copy & paste'
+    div.innerHTML = ''
 } else if (rnd < 0.5) {
     div.style.fontSize = '13px'
     div.style.letterSpacing = "0.5px"
-    div.innerHTML = 'Welcome Back to Planet Earth!'
+    div.innerHTML = 'Welcome back, friend!'
 } else if (rnd < 0.75) {
     div.style.fontSize = '7.4px'
-    div.innerHTML = 'Now how\'s that for "disruption," five-letter dotcoms?'
+    div.innerHTML = ''
 } else {
     div.style.fontSize = '10.9px'
-    div.innerHTML = "Maybe I give it to you all on a dime"
+    div.innerHTML = 'Have fun, be safe!'
 }
-
-// document.title = 'textz.com · ' + div.innerHTML
