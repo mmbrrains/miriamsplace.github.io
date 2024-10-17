7286e8822e6f00892d62b2a56f5a320297a0dafb
mmbrrains <miriamrains0219@gmail.com>
2024-10-17 16:17:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-let div = document.getElementById('tagline')
-let rnd = Math.random()
-
-/*
-if (rnd < 0.25) {
-    div.style.fontSize = '11.8px'
-    div.innerHTML = ''
-} else if (rnd < 0.5) {
-    div.style.fontSize = '13px'
-    div.style.letterSpacing = "0.5px"
-    div.innerHTML = 'Welcome back, friend!'
-} else if (rnd < 0.75) {
-    div.style.fontSize = '7.4px'
-    div.innerHTML = ''
-} else {
-    div.style.fontSize = '10.9px'
-    div.innerHTML = 'Have fun, be safe!'
-}
-*/
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.classList.add('ready');
+});
