425e31aaca6ed5d70b96731ad87e140a120115fb
mmbrrains <miriamrains0219@gmail.com>
2024-11-15 09:29:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 window.onload = function() {
     document.body.classList.add('ready');
 };
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  document.getElementById('feedback').style.display = 'block';
+
+  document.getElementById('contactForm').reset();
+});
