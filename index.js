2c3728654760fc64a9b8cfe131181c0dfa23a651
miri <miriamrains0219@gmail.com>
2024-11-15 16:13:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ window.onload = function() {
     document.body.classList.add('ready');
 };
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-  event.preventDefault();
+document.getElementById('button').addEventListener('click', function(event) {
 
-  document.getElementById('feedback').style.opacity = '1';
+  document.getElementById('email').style.opacity = '1';
 
-  document.getElementById('contactForm').reset();
 });
