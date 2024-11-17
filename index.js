9ce5ce789ac4b3cc7d64d14b29e80b0fc1f8d925
miri <miriamrains0219@gmail.com>
2024-11-17 01:57:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
                     this.classList.add('clicked');
                     setTimeout(() => {
                         window.location.href = href;
-                    }, 500); // Adjust this timeout to match the transition duration
+                    }, 500);
                 });
         });
 });
